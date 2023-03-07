fe25d9d5abe1355d15db10a73a13ff98aee4b20d
Mia-Oh <songmiaoh@gmail.com>
2023-03-07 17:34:35 +0900

    fix:주석 추가

--- src/Style/MenuStyle.js
+++ src/Style/MenuStyle.js
@@ -2,5 +2,5 @@ import styled from 'styled-components';
 import { Button } from 'antd';
 
 export const MenuButton = styled(Button)`
-  background-color: var(--coz-purple-400);
+  background-color: var(--mint-600);
 `;
